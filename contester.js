b8fc2a7db5485adfcb99a0a777842a001eafbad8
chaseadams509 <chase.adams509@gmail.com>
2020-12-27 23:34:48 -0800

    Update contester.js

--- contester.js
+++ contester.js
@@ -176,25 +176,23 @@ class Simulator {
 
   getAvailableMoves(pokemon_resource) {
     let self = this;
-    return pokemon_resource.moves.reduce(function(total, movename) {
+    return pokemon_resource.moves.reduce(async function(total, movename) {
       if (!self.checkLearnable(movename, 'RSEFLCX')) {
         return total;
       }
-      return (async function() {
-        let move = await self.P.getMoveByName(movename.move.name);
-        if (move.contest_effect == null) {
-          console.log("Contest effect is missing");
-          return total;
-        }
-        console.log("Move is eligible for contest");
-        let move_obj = contest_effect_to_move[move.contest_effect.url].copy(move.name, move.contest_type.name);
-        if (move.contest_combos && move.contest_combos.normal && move.contest_combos.normal.use_before) {
-          move_obj.contest_combos = move.contest_combos.normal.use_before.reduce(function(all_combos, c) {
-            return all_combos.add(c.name);
-          }, new Set());
-        }
-        return total.push(move_obj);
-      })();
+      let move = await self.P.getMoveByName(movename.move.name);
+      if (move.contest_effect == null) {
+        console.log("Contest effect is missing");
+        return total;
+      }
+      console.log("Move is eligible for contest");
+      let move_obj = contest_effect_to_move[move.contest_effect.url].copy(move.name, move.contest_type.name);
+      if (move.contest_combos && move.contest_combos.normal && move.contest_combos.normal.use_before) {
+        move_obj.contest_combos = move.contest_combos.normal.use_before.reduce(function(all_combos, c) {
+          return all_combos.add(c.name);
+        }, new Set());
+      }
+      return total.push(move_obj);
     }, new Array());
   }
   
