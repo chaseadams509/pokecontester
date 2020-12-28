672abf62e67160a347532195f2d9d08bc8b68bbc
chaseadams509 <chase.adams509@gmail.com>
2020-12-28 00:28:05 -0800

    Update contester.js

--- contester.js
+++ contester.js
@@ -176,36 +176,54 @@ class Simulator {
 
   getAvailableMoves(pokemon_resource) {
     console.log("Have this many moves:", pokemon_resource.moves);
-    return pokemon_resource.moves.reduce(async(total, movename) => {
-      console.log("Received:", total, movename.move.name);
-      
-      if (!this.checkLearnable(movename, 'RSEFLCX')) {
-        return total;
-      }
-      let move = await this.P.getMoveByName(movename.move.name);
-      if (move.contest_effect == null) {
-        console.log("Contest effect is missing");
-        return total;
-      }
-      console.log("Move is eligible for contest");
-      let move_obj = contest_effect_to_move[move.contest_effect.url].copy(move.name, move.contest_type.name);
-      if (move.contest_combos && move.contest_combos.normal && move.contest_combos.normal.use_before) {
-        move_obj.contest_combos = move.contest_combos.normal.use_before.reduce(function(all_combos, c) {
-          return all_combos.add(c.name);
-        }, new Set());
-      }
-      console.log("Concatenating:", total, move.name);
-      total.push(move_obj);
-      return total;
-    }, new Array());
+    let movePromises = pokemon_resource.moves.
+    filter(movename => this.checkLearnable(movename, 'RSEFLCX')).
+    map(movename => this.P.getMoveByName(movename.move.name));
+    return Promise.all(movePromises).then(moves => {
+      return moves.
+      filter(move => move.contest_effect != null).
+      map(move => {
+        let move_obj = contest_effect_to_move[move.contest_effect.url].copy(move.name, move.contest_type.name);
+        if (move.contest_combos && move.contest_combos.normal && move.contest_combos.normal.use_before) {
+          move_obj.contest_combos = move.contest_combos.normal.use_before.reduce(function(all_combos, c) {
+            return all_combos.add(c.name);
+          }, new Set());
+        }
+        return move_obj;
+      });
+    });
   }
+      
+      
+//     return pokemon_resource.moves.reduce(async(total, movename) => {
+//       console.log("Received:", total, movename.move.name);  
+      
+//       if (!this.checkLearnable(movename, 'RSEFLCX')) {
+//         return total;
+//       }
+//       let move = await this.P.getMoveByName(movename.move.name);
+//       if (move.contest_effect == null) {
+//         console.log("Contest effect is missing");
+//         return total;
+//       }
+//       console.log("Move is eligible for contest");
+//       let move_obj = contest_effect_to_move[move.contest_effect.url].copy(move.name, move.contest_type.name);
+//       if (move.contest_combos && move.contest_combos.normal && move.contest_combos.normal.use_before) {
+//         move_obj.contest_combos = move.contest_combos.normal.use_before.reduce(function(all_combos, c) {
+//           return all_combos.add(c.name);
+//         }, new Set());
+//       }
+//       console.log("Concatenating:", total, move.name);
+//       total.push(move_obj);
+//       return total;
+//     }, new Array());
+//   }
   
   calculateContest(pokemon) {
-    let self = this;
-    self.P.getPokemonByName(pokemon) // with Promise
-    .then(function (pokemon_resource) {
+    this.P.getPokemonByName(pokemon) // with Promise
+    .then((pokemon_resource) => {
       console.log(pokemon_resource);
-      let available_moves = self.getAvailableMoves(pokemon_resource);
+      let available_moves = this.getAvailableMoves(pokemon_resource);
       console.log(available_moves.length);
       });
   }
