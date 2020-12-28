e23008f923a2c1b571faa42cf23b67a79ac577e2
chaseadams509 <chase.adams509@gmail.com>
2020-12-27 17:57:01 -0800

    Update contester.js

--- contester.js
+++ contester.js
@@ -18,17 +18,17 @@ class Move {
   
   getCopy(name) {
         return new Move({
-        appeal = this.appeal,
-        name = name,
-        description = this.description,
-        bonus_order = this.bonus_order,
-        priority = this.priority,
-        no_more = this.no_more,
-        no_penalty = this.no_penalty,
-        compound_condition = this.compound_condition,
-        star_additions = this.star_additions,
-        jam_factor = this.jam_factor,
-        contest_combos = this.contest_combos}
+        appeal: this.appeal,
+        name: name,
+        description: this.description,
+        bonus_order: this.bonus_order,
+        priority: this.priority,
+        no_more: this.no_more,
+        no_penalty: this.no_penalty,
+        compound_condition: this.compound_condition,
+        star_additions: this.star_additions,
+        jam_factor: this.jam_factor,
+        contest_combos: this.contest_combos}
         );
   }
 }
@@ -37,7 +37,7 @@ class Move {
 document.addEventListener('DOMContentLoaded', (event) => {
   const P = new Pokedex.Pokedex()
   const form = document.getElementById('pokeform');
-  const nullMove = new Move({name="null"});
+  const nullMove = new Move({name: "null"});
   consol.log(nullMove.name);
 
   function computeSubmit(event) {
